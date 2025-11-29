660f62ba36fea0cca57d63c56486c223a8e9ceca
clozyfps <collegefootball081@gmail.com>
2025-11-29 13:59:07 -0500

    hey

---
+++ src/main/java/net/clozy/YaibaDataGenerator.java
@@ -0,0 +1,11 @@
+package net.clozy;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class YaibaDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
